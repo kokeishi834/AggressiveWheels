7663b8d1b34e18fe67d35603c7757daf994c5513
iSO1 <yu_ki0215aki@icloud.com>
2020-12-25 10:33:48 +0900

    Nav Mesh全般の実装,天球の実装
    CPUに目的地へ向かう動きを実装、StageSTSceneに機能が全部入っているが基本は敵にAddConponentで「Nav Mesh Agent」と「Nav Mesh Test」のスクリプトを追加してインスペクターを設定すれば動くはず

---
+++ Assets/Scripts/ConeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConeController : MonoBehaviour
+{
+    Time time;
+    float pos_y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos_y = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //上下させる部分（跳ねる感じを出したver）
+        float sin = Mathf.Sin(Time.time);
+
+        this.transform.Rotate(0.0f, 1.0f, 0.0f);
+
+        this.transform.position = new Vector3(transform.position.x,
+            sin / 4 + pos_y, transform.position.z);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //ここにギミック発動の処理を入れる
+        }
+    }
+}
