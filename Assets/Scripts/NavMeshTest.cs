271f844dc9127db06c9a57f8a2455a32b7c87659
iSO1 <yu_ki0215aki@icloud.com>
2021-01-18 16:14:26 +0900

    ステージ３に敵を１２体配置
    ステージ３に敵を１２体配置してそれぞれ自走するように変更
    NavMeshTestのスクリプトを若干変更（動作に変更なし）
    ※敵は作業の都合上「A」のスクリプトをチェック外した状態になってるので使う場合は変更お願いします

--- Assets/Scripts/NavMeshTest.cs
+++ Assets/Scripts/NavMeshTest.cs
@@ -10,28 +10,32 @@ public class NavMeshTest : MonoBehaviour
     public float range = 0;          //到達判定の範囲
     public int goal = 0;             //目的地の番号
     public float agent_range = 0.5f; //プレイヤーの半径
+
+    
+    float distance;                  //目的地までの距離
     // Start is called before the first frame update
     void Start()
     {
         //目的地を設定してあげる
         agent.SetDestination(target[goal].transform.position);
+
+        distance = Vector2.Distance(new Vector2(transform.position.x,
+            transform.position.z),new Vector2(target[goal].transform.position.x,
+            target[goal].transform.position.z));
+        
     }
 
     // Update is called once per frame
     void Update()
     { 
         //目的地付近に到達したら次に向かう
-        float dis_x = transform.position.x - target[goal].transform.position.x;
-        float dis_z = transform.position.z - target[goal].transform.position.z;
-
-        //Debug.Log(Mathf.Sqrt(dis_x * dis_x + dis_z * dis_z));
-        if (agent_range + range >= Mathf.Sqrt(dis_x * dis_x + dis_z* dis_z))
+        if (agent_range + range >= distance)
         {
             //Debug.Log(target.Length);
             {
                 if (goal != target.Length - 1)
                 {
-                    goal++;    
+                    goal++;
                 }
                 else
                 {
@@ -41,5 +45,10 @@ public class NavMeshTest : MonoBehaviour
                 agent.SetDestination(target[goal].transform.position);
             }
         }
+        
+
+        distance = Vector2.Distance(new Vector2(transform.position.x,
+            transform.position.z),new Vector2(target[goal].transform.position.x,
+            target[goal].transform.position.z));
     }
 }
