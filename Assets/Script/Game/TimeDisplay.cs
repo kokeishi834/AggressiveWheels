e8adaf87d418d17c3413e592600dfcf633773edb
iSO1 <yu_ki0215aki@icloud.com>
2021-02-04 16:10:59 +0900

    スコアの復活
    スコアが再度加算されるように調整

---
+++ Assets/Script/Game/TimeDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TimeDisplay : MonoBehaviour
+{
+    public GameObject Time = null;
+    int count;//タイムで表示
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count++;
+        int now_time = count / 60;
+        Text time_text = Time.GetComponent<Text>();
+        // テキストの表示を入れ替える
+        time_text.text = "Time・・・" + now_time.ToString();
+    }
+}
